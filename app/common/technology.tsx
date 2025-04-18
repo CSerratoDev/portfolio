7313de171a5d87b78a19c6326fe397bdc02b4fce
Alexis Serrato <cserrato.dev@gmail.com>
2025-04-18 00:28:22 -0600

    Added a list of mastered programming languages

---
+++ app/common/technology.tsx
@@ -0,0 +1,23 @@
+export default function Technology() {
+    return (
+        <div className="tag-list">
+            <div className="inner">
+                <div className="tag"><span>#</span>
+                Typescript</div>
+                <div className="tag"><span>#</span>
+                Html5</div>
+                <div className="tag"><span>#</span>
+                CSS</div>
+                <div className="tag"><span>#</span>
+                Tailwind</div>
+                <div className="tag"><span>#</span>
+                UI/UX</div>
+                <div className="tag"><span>#</span>
+                Python</div>
+                <div className="tag"><span>#</span>
+                OracleApex</div>
+            </div>
+            <div className="fade flex flex-wrap"></div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import AboutMe from "./profile/AboutMe";
 import Achievements from "./profile/Achievements";
 import Projects from "./profile/Projects";
 import Avatar from "./common/avatar";
+import Technology from "./common/technology";
 
 export default function Home() {
   return (
     <main>
       <Header/>
-
-      <div className="flex p-6">
-        <AboutMe/>
-        <Avatar/>
-      </div>
+      <AboutMe/>
+      <Avatar/>
+      <section className="justify-center flex">
+      <Technology/>
+      </section>
       <Projects/>
       <Achievements/>
     </main>
