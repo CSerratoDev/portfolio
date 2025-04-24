76acea2fef50fffb1129dc41b1760291dfc72d3e
Alexis Serrato <cserrato.dev@gmail.com>
2025-04-23 18:26:24 -0600

    Updating the footer to a Typescript Interface

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,12 +1,22 @@
+import { FC } from "react";
 import { LiaCopyrightSolid } from "react-icons/lia";
 
-export default function Footer() {
-    return(
+interface FooterProps {
+    year?: number;
+    owner?: string;
+}
+
+const Footer : FC<FooterProps> = ({
+    year = new Date().getFullYear(),
+    owner = "Alexis Serrato"
+}) => {
+    return (
         <footer className="h-auto flex bg-[#252525] text-white text-center py-4 justify-center">
                 <p className="flex items-center gap-1">
-                    2025 My website. All Rights Reserved Alexis Serrato 
+                    {year} My website. All Rights Reserved {owner} 
                     <LiaCopyrightSolid/>
                 </p>
         </footer>
     );
 }
+export default Footer;
